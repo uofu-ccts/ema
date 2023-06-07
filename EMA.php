b7fb52c84769869be11cfc663512c042dd01d1bd
Sumon Chattopadhyay <sumon.chattopadhyay@utah.edu>
2023-06-07 08:52:39 -0700

    Adds debug method to class
    Plugin page now shows REDCap nav (added to config.json)

--- EMA.php
+++ EMA.php
@@ -4,5 +4,23 @@ namespace Utah\EMA;
 
 class EMA extends \ExternalModules\AbstractExternalModule
 {
+  
+  public $project_id = null;
+  public $username = USERID; // a REDCap constant; see redcap_info() output on the dev doc page
 
+  public function __construct() {
+
+    parent::__construct(); // call parent (AbstractExternalModule) constructor
+
+    $this->project_id = $this->getProjectId(); // defined in AbstractExternalModule; will return project_id or null
+
+ }
+
+  function debug_to_console($data, $text='Debug Object',) {
+    $output = $data;
+    if (is_array($output))
+        $output = implode(',', $output);
+
+    echo "<script>console.log('" . $text . $output . "' );</script>";
+  }
 }

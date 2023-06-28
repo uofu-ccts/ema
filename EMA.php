792a6bac0ae1930941b36b64a5c5369b1615857d
Sumon Chattopadhyay <sumon.chattopadhyay@utah.edu>
2023-06-28 15:10:58 -0700

    refactor: moved schedule generation to module class

--- EMA.php
+++ EMA.php
@@ -2,19 +2,28 @@
 
 namespace Utah\EMA;
 
-class EMA extends \ExternalModules\AbstractExternalModule
+use ExternalModules\AbstractExternalModule;
+
+class EMA extends AbstractExternalModule
 {
   
   public $project_id = null;
   public $username = USERID; // a REDCap constant; see redcap_info() output on the dev doc page
+  public $errorLog = [];
 
-  // variables to store settings
-  // this is where slider variables and settings are configured
-  public $configured_variables = [];
-
-  //list of fields
-  public $survey_start_fields = [];
-  public $survey_save_fields = [];
+  //list of fields, will be populated by getFieldNames
+  public $setupCompletionField = "";
+  public $surveyStartField = "";
+  public $surveyStatusField = "";
+  public $surveyDurationField = "";
+  public $scheduleCompletionField = "";
+  public $sendDateField = "";
+  public $sendTimeFields = [];
+  public $sendFlagFields = [];
+  public $startRangeFields = [];
+  public $expireRangeFields = [];
+  public $expireTimeFields = [];
+  public $expireFlagFields = [];
 
   public function __construct() {
 
@@ -24,8 +33,111 @@ class EMA extends \ExternalModules\AbstractExternalModule
 
   }
 
-  function generateSchedules($records) {
+  function getFieldNames($project_id) {
+    $this->setupCompletionField = implode($this->getProjectSetting('setup-completion', $project_id));
+    $this->surveyStartField = implode($this->getProjectSetting('start-date', $project_id));
+    $this->surveyStatusField = implode($this->getProjectSetting('status', $project_id));
+    $this->surveyDurationField = implode($this->getProjectSetting('num-days', $project_id));
+    $this->scheduleCompletionField = implode($this->getProjectSetting('schedule-completion', $project_id));
+    $this->sendDateField = implode($this->getProjectSetting('send-date', $project_id));
+    $this->sendTimeFields = $this->getProjectSetting('send-time', $project_id)[0];
+    $this->sendFlagFields = $this->getProjectSetting('send-flag', $project_id)[0];
+    $this->startRangeFields = $this->getProjectSetting('start-range', $project_id)[0];
+    $this->expireRangeFields = $this->getProjectSetting('expire-range', $project_id)[0];
+    $this->expireTimeFields = $this->getProjectSetting('expire-time', $project_id)[0];
+    $this->expireFlagFields = $this->getProjectSetting('expire-flag', $project_id)[0];
+  }
 
+  function generateSchedules($records, $project_id, $surveyStartField, $surveyDurationField, $startRangeFields, $expireRangeFields, $sendDateField, $sendTimeFields, $sendFlagFields, $expireTimeFields, $expireFlagFields, $errorLog) {
+    $dataToSave = [];
+    foreach ($records as $record) {
+
+      $this->debug_to_console($project_id, "project_id");
+      $this->debug_to_console($record, "record");
+      $this->debug_to_console($surveyStartField, "surveystartfield");
+      $this->debug_to_console($surveyDurationField, "surveydurationfield");
+
+      $dateParams = $this->getDateParams($project_id, $record, $surveyStartField, $surveyDurationField);
+
+      $this->debug_to_console($dateParams, "Date params");
+
+      if (!$dateParams[$surveyDurationField]) {
+        array_push($errorLog, "$surveyDurationField is missing for record $record. Moving on to next record...");
+        continue;
+      }
+
+      if (!$dateParams[$surveyStartField]) {
+        array_push($errorLog, "$surveyStartField is missing for record $record. Moving on to next record...");
+        continue;
+      }
+
+      $startParams = $this->getTimeParams($project_id, $record, $startRangeFields);
+
+      if (count($startParams) != count($startRangeFields)) {
+        $errorText = implode($startRangeFields);
+        array_push($errorLog, "One of the $errorText is missing for record $record. Moving on to next record...");
+        continue;
+      }
+
+      $expireParams = $this->getTimeParams($project_id, $record, $expireRangeFields);
+
+      if (count($expireParams) != count($expireRangeFields)) {
+        $errorText = implode($expireRangeFields);
+        array_push($errorLog, "One of the $errorText is missing for record $record. Moving on to next record...");
+        continue;
+      }
+
+      $numDays = $dateParams[$surveyDurationField];
+      $startDate = new \DateTimeImmutable($dateParams[$surveyStartField]);
+
+      print_r('Current record: ' . $record . '<br>');
+
+      $dataToSave[$record] = [];
+
+      for ( $currentDay=1; $currentDay <= $numDays; $currentDay++ ) {
+
+        $currentRedcapEvent = 'day_' . $currentDay . '_arm_1';
+        $currentSurveyDate = $startDate->add(new \DateInterval('P' . $currentDay-1 . 'D'))->format('Y-m-d');
+
+        $unique_event_id = \REDCap::getEventIdFromUniqueEvent($currentRedcapEvent);
+
+        $dataToSave[$record][$unique_event_id] = [];
+
+        $dataToSave[$record][$unique_event_id][$sendDateField] = $currentSurveyDate;
+
+        print_r('Current event: ' . $currentRedcapEvent);
+        print_r("<br>");
+        print_r('Scheduled survey date: ' . $currentSurveyDate);
+        print_r("<br>");
+
+        for ( $currentSurvey=0; $currentSurvey < count($sendTimeFields); $currentSurvey++ ) {
+          $startTime = $startParams[$startRangeFields[$currentSurvey]];
+          $sendFlag = 0; // 1 = true, 0 = false
+          $expireTime = $expireParams[$expireRangeFields[$currentSurvey]];
+          $expireFlag = 0; // 1 = true, 0 = false
+
+          $sendTime = $this->generateRandomTime($startTime, $expireTime);
+
+          $dataToSave[$record][$unique_event_id][$sendTimeFields[$currentSurvey]] = $sendTime;
+          $dataToSave[$record][$unique_event_id][$sendFlagFields[$currentSurvey]] = $sendFlag;
+          $dataToSave[$record][$unique_event_id][$expireTimeFields[$currentSurvey]] = $expireTime;
+          $dataToSave[$record][$unique_event_id][$expireFlagFields[$currentSurvey]] = $expireFlag;
+
+          print_r('Time to send survey: ' . $sendTime);
+          print_r("<br>");
+          print_r('Flag to send survey: ' . $sendFlag);
+          print_r("<br>");
+          print_r('Time to expire survey: ' . $expireTime);
+          print_r("<br>");
+          print_r('Flag to expire survey: ' . $expireFlag);
+          print_r("<br>");
+        }
+
+        print_r("<br>");
+      }
+    }
+
+    return $dataToSave;
   }
 
   function getDateParams($project_id, $record, $surveyStartField, $surveyDurationField) {
@@ -58,7 +170,7 @@ class EMA extends \ExternalModules\AbstractExternalModule
     return $data[$record][$event_id];
   }
 
-  /* 
+  /*
     Returns an array of records that need a survey schedule generated
     Uses getRecordsWithSetup and getRecordsWithSchedule, and finds list of records that have a complete Survey Setup instrument, but blank Survey Schedule instruments
   */
@@ -124,6 +236,36 @@ class EMA extends \ExternalModules\AbstractExternalModule
     $randomTimestamp = mt_rand($startTimestamp, $endTimestamp);
   
     return date('H:i', $randomTimestamp);
+  }
+
+  function saveSchedules($dataToSave) {
+    $params = array(
+      'dataFormat' => 'array',
+      'data' => $dataToSave,
+      'overwriteBehavior' => 'normal',
+      'dateFormat' => 'YMD'
+    );
+    
+    $response = \REDCap::saveData($params);
+
+    return $response;
+  }
+
+  /** 
+   * @param array $cronAttributes A copy of the cron's configuration block from config.json.
+   */
+  function cron($cronInfo){
+    foreach($this->getProjectsWithModuleEnabled() as $localProjectId){
+      $this->setProjectId($localProjectId);
+  
+      // If setProjectId() is not available in your REDCap version, the following will have the same effect:
+      $_GET['pid'] = $localProjectId;
+  
+      // Project specific method calls go here.
+      $someValue = $this->getProjectSetting('some_key');
+    }
+  
+    return "The \"{$cronInfo['cron_description']}\" cron job completed successfully.";
   }
 
   function debug_to_console($data, $text='Debug Object',) {
--- schedule_generator.php
+++ schedule_generator.php
@@ -4,123 +4,32 @@ use RCView;
 
 echo RCView::h4([], "Schedule Generator");
 
-$errorLog = [];
+$module->getFieldNames($module->project_id);
 
-$setupCompletionField = implode($module->getProjectSetting('setup-completion', $project_id));
-$surveyStartField = implode($module->getProjectSetting('start-date', $project_id));
-$surveyStatusField = implode($module->getProjectSetting('status', $project_id));
-$surveyDurationField = implode($module->getProjectSetting('num-days', $project_id));
-$scheduleCompletionField = implode($module->getProjectSetting('schedule-completion', $project_id));
-$sendDateField = implode($module->getProjectSetting('send-date', $project_id));
-$sendTimeFields = $module->getProjectSetting('send-time', $project_id)[0];
-$sendFlagFields = $module->getProjectSetting('send-flag', $project_id)[0];
-$startRangeFields = $module->getProjectSetting('start-range', $project_id)[0];
-$expireRangeFields = $module->getProjectSetting('expire-range', $project_id)[0];
-$expireTimeFields = $module->getProjectSetting('expire-time', $project_id)[0];
-$expireFlagFields = $module->getProjectSetting('expire-flag', $project_id)[0];
+$records = $module->getRecordsToSchedule($module->project_id, 
+                                          $module->setupCompletionField, 
+                                          $module->scheduleCompletionField, 
+                                          $module->surveyStatusField);
 
-$records = $module->getRecordsToSchedule($project_id, $setupCompletionField, $scheduleCompletionField, $surveyStatusField);
 
-/* $redcap_data is structured as:
-    [
-        record_id => [
-            event_id => [
-                field_name => value,
-                ...
-                ],
-            ...
-            ],
-        ...
-    ]
-*/
-$dataToSave = [];
-foreach ($records as $record) {
+$dataToSave = $module->generateSchedules($records, 
+                                          $module->project_id, 
+                                          $module->surveyStartField, 
+                                          $module->surveyDurationField, 
+                                          $module->startRangeFields, 
+                                          $module->expireRangeFields, 
+                                          $module->sendDateField, 
+                                          $module->sendTimeFields, 
+                                          $module->sendFlagFields, 
+                                          $module->expireTimeFields, 
+                                          $module->expireFlagFields,
+                                          $module->errorLog
+                                        );
 
-  $dateParams = $module->getDateParams($project_id, $record, $surveyStartField, $surveyDurationField);
-
-  if (!$dateParams[$surveyDurationField]) {
-    array_push($errorLog, "$surveyDurationField is missing for record $record. Moving on to next record...");
-    continue;
-  }
-
-  if (!$dateParams[$surveyStartField]) {
-    array_push($errorLog, "$surveyStartField is missing for record $record. Moving on to next record...");
-    continue;
-  }
-
-  $startParams = $module->getTimeParams($project_id, $record, $startRangeFields);
-
-  if (count($startParams) != count($startRangeFields)) {
-    $errorText = implode($startRangeFields);
-    array_push($errorLog, "One of the $errorText is missing for record $record. Moving on to next record...");
-    continue;
-  }
-
-  $expireParams = $module->getTimeParams($project_id, $record, $expireRangeFields);
-
-  if (count($expireParams) != count($expireRangeFields)) {
-    $errorText = implode($expireRangeFields);
-    array_push($errorLog, "One of the $errorText is missing for record $record. Moving on to next record...");
-    continue;
-  }
-
-  $numDays = $dateParams[$surveyDurationField];
-  $startDate = new DateTimeImmutable($dateParams[$surveyStartField]);
-
-  print_r('Current record: ' . $record . '<br>');
-
-  $dataToSave[$record] = [];
-
-  for ( $currentDay=1; $currentDay <= $numDays; $currentDay++ ) {
-
-    $currentRedcapEvent = 'day_' . $currentDay . '_arm_1';
-    $currentSurveyDate = $startDate->add(new DateInterval('P' . $currentDay-1 . 'D'))->format('Y-m-d');
-
-    $unique_event_id = \REDCap::getEventIdFromUniqueEvent($currentRedcapEvent);
-
-    $dataToSave[$record][$unique_event_id] = [];
-
-    $dataToSave[$record][$unique_event_id][$sendDateField] = $currentSurveyDate;
-
-    print_r('Current event: ' . $currentRedcapEvent);
-    print_r("<br>");
-    print_r('Scheduled survey date: ' . $currentSurveyDate);
-    print_r("<br>");
-
-    for ( $currentSurvey=0; $currentSurvey < count($sendTimeFields); $currentSurvey++ ) {
-      $startTime = $startParams[$startRangeFields[$currentSurvey]];
-      $sendFlag = 0; // 1 = true, 0 = false
-      $expireTime = $expireParams[$expireRangeFields[$currentSurvey]];
-      $expireFlag = 0; // 1 = true, 0 = false
-
-      $sendTime = $module->generateRandomTime($startTime, $expireTime);
-
-      $dataToSave[$record][$unique_event_id][$sendTimeFields[$currentSurvey]] = $sendTime;
-      $dataToSave[$record][$unique_event_id][$sendFlagFields[$currentSurvey]] = $sendFlag;
-      $dataToSave[$record][$unique_event_id][$expireTimeFields[$currentSurvey]] = $expireTime;
-      $dataToSave[$record][$unique_event_id][$expireFlagFields[$currentSurvey]] = $expireFlag;
-
-      print_r('Time to send survey: ' . $sendTime);
-      print_r("<br>");
-      print_r('Flag to send survey: ' . $sendFlag);
-      print_r("<br>");
-      print_r('Time to expire survey: ' . $expireTime);
-      print_r("<br>");
-      print_r('Flag to expire survey: ' . $expireFlag);
-      print_r("<br>");
-    }
-
-    print_r("<br>");
-  }
-}
-
-$params = array(
-  'dataFormat' => 'array',
-  'data' => $dataToSave,
-  'overwriteBehavior' => 'normal',
-  'dateFormat' => 'YMD'
-);
-
-$response = \REDCap::saveData($params);
+$response = $module->saveSchedules($dataToSave);
 
 print_r($response);
+
+?>
+
+<button type="button" class="btn btn-primary">Primary</button>
